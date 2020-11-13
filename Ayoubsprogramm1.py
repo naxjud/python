77f4e47599ab8affb529660decb64ab394fd4daa
Moussa Zraidi <mzraidi@gmail.com>
2020-11-13 14:11:30 +0100

    testing for branch delete

--- Ayoubsprogramm1.py
+++ Ayoubsprogramm1.py
@@ -2,3 +2,4 @@ print("Mooooooin Meister!")
 topf = "Lego set"
 print(topf)
 print("Hallo ich programmiere gerade. Wer kann das auch?°,,,,°")
+print("-------------------------------------------------------")
